715f88605db0b7b0ba145a1b9805db23c778a490
limforever00 <limforever00@gmail.com>
2023-10-13 22:24:38 +0900

    translate_project add

---
+++ translate_project/db_settings.py
@@ -0,0 +1,15 @@
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.mysql",  # 엔진
+        "NAME": "translate_db",  # 데이터베이스 이름
+        "USER": "root",  # 사용자
+        "PASSWORD": "1234",  # 비밀번호
+        "HOST": "localhost",  # 호스트
+        "PORT": "3306",  # 포트번호
+    }
+}
+
+# SECRET_KEY
+# 1. settings.py의 secret_key는 주석처리
+# 2. 복사해서 여기에서 사용
+SECRET_KEY = 'django-insecure-y6jkwlll3f^8j@g!ae4wbs5jc-$lqmm)15#6w81#+qu(d4fa7('
---
+++ translate_project/translate_app/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+# urls에 path를 동록하여 사용할 페이지의 링크를 지정한다
+
+urlpatterns = [
+    path('', views.index, name='index'),    
+]
