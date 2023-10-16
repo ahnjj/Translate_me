3e584bbbbed0bf2aa39f6c3a9cdbf106de82bddb
lifelong-learner-dev <dev.lifelong.learner.dev@gmail.com>
2023-10-16 10:45:11 +0300

    board_app_create_forms_models_urls

--- translate_project/board_app/models.py
+++ translate_project/board_app/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
 # Create your models here.
+class Board(models.Model):
+    board_id = models.AutoField(primary_key=True)
+    board_title = models.CharField(max_length=100, blank=True, null=True)
+    board_main_txt = models.TextField(blank=True, null=True)
+    category = models.ForeignKey('Category', models.DO_NOTHING, blank=True, null=True)
+    language = models.ForeignKey('LanguageCode', models.DO_NOTHING, blank=True, null=True)
+    user = models.ForeignKey('UserInfo', models.DO_NOTHING, blank=True, null=True)
+    reg_date = models.DateField(blank=True, null=True)
+    lst_chg_date = models.DateField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'board'
+
+
+class BoardComment(models.Model):
+    comment_id = models.AutoField(primary_key=True)  # The composite primary key (comment_id, board_id, user_id) found, that is not supported. The first column is selected.
+    board = models.ForeignKey(Board, models.DO_NOTHING)
+    user = models.ForeignKey('UserInfo', models.DO_NOTHING)
+    comment_text = models.CharField(max_length=200, blank=True, null=True)
+    reg_date = models.DateField(blank=True, null=True)
+    lst_chg_date = models.DateField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'board_comment'
+        unique_together = (('comment_id', 'board', 'user'),)
+
+class Category(models.Model):
+    category_id = models.CharField(primary_key=True, max_length=10)
+    category_name = models.CharField(max_length=30, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'category'
+
+class LanguageCode(models.Model):
+    language_id = models.CharField(primary_key=True, max_length=10)
+    language_name = models.CharField(max_length=30, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'language_code'
--- translate_project/board_app/urls.py
+++ translate_project/board_app/urls.py
@@ -4,4 +4,12 @@ from . import views
 # urls에 path를 동록하여 사용할 페이지의 링크를 지정한다
 
 urlpatterns = [
+    path('board/list/', views.board_list, name='board_list'),
+    #http://127.0.0.1:8000/board/list/
+    path('board/detail/<str:boardno>/', views.board_detail, name='board_detail'),
+    path('board/insert/', views.board_insert, name='board_insert'),
+    path('board/update/<str:boardno>/', views.board_update, name='board_update'),
+    path('board/delete/<str:boardno>/', views.board_delete, name='board_delete'),
+    path('board/search_form/', views.board_search_form, name='board_search_form'),
+    path('board/search/', views.board_search, name='board_search'),
 ]
