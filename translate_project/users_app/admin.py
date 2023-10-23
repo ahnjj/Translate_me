429af599e0e1f9f7ffe5b7859e828458dd65ff57
jeong_ahn <ahnjj.dev@gmail.com>
2023-10-23 13:47:31 +0900

    aj_develop

--- translate_project/users_app/admin.py
+++ translate_project/users_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['username','email','website_url', 'user_language','user_level','avatar','is_active', 'is_staff', 'is_superuser']
