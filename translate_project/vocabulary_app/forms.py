0db7bb1b077740e515ebaba1a433e1e0ef26bde4
S.H.Jang <jsh64566@gmail.com>
2023-10-24 14:56:10 +0900

    jsh vocabulary_app modify 1

--- translate_project/vocabulary_app/forms.py
+++ translate_project/vocabulary_app/forms.py
@@ -12,6 +12,28 @@ class Vocabulary_Form(forms.ModelForm):
             'language_id'
         )
 
+        widgets = {
+            'vocabulary_name': forms.TextInput(attrs={
+                'class': "form-control",
+                'style': 'width: 80%;',
+                'placeholder': '단어 입력'
+                }),
+            'vocabulary_meaning': forms.TextInput(attrs={
+                'class': "form-control",
+                'style': 'width: 100%; height: 100px',
+                'placeholder': '뜻 입력'
+                }),
+            'vocabulary_level': forms.NumberInput(attrs={
+                'class': "form-control",
+                'style': 'width: 60%;',
+                'placeholder': '레벨 입력'
+                }),
+            'language_id': forms.Select(attrs={
+                'class': "form-select",
+                'style': 'width: 60%;'
+            }),
+        }
+
         labels = {
             'vocabulary_name':'단어', 
             'vocabulary_meaning':'뜻',
