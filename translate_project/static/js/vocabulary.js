f9bd66cc8b9d0c00ed9e5ca49684cee125df5ceb
S.H.Jang <jsh64566@gmail.com>
2023-10-25 16:14:51 +0900

    jsh voca&board redesign 08

--- translate_project/static/js/vocabulary.js
+++ translate_project/static/js/vocabulary.js
@@ -10,4 +10,26 @@ function toggleMeaning(button) {
             button.textContent = "보기";
         }
     }
-}
+};
+
+$(document).ready(function(){
+    $("a.to-another-page").on("click", function(){
+        event.preventDefault()
+        const total_pages = $("#num-of-pages").text();
+        while (true){
+            page = prompt("이동할 페이지를 입력하세요(1 ~ " + total_pages + ")");
+            if (page == null){
+                break;
+            } else {
+                page = parseInt(page);
+                if (page<=parseInt(total_pages)&&page>=1){
+                    window.location.href = "http://127.0.0.1:8000/vocabulary_list/?page=" + String(page)
+                    break;
+                } else {
+                    alert("1 ~ " + total_pages + " 사이의 숫자를 입력하세요");
+                };
+            };
+        };
+        
+    });
+});
